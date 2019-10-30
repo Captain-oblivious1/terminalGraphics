c4c38e2f6a4498d6a00b5c96d2737dc1b5ee95b9
Duane Knesek <duane.knesek@gmail.com>
2019-10-29 22:03:21 -0500

    Box has border and can be moved

--- TerminalGraphics.py
+++ TerminalGraphics.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 import curses
+import math
 from curses.textpad import Textbox, rectangle
 
 # §§§§§  
@@ -16,10 +17,10 @@ from curses.textpad import Textbox, rectangle
 #                  │──────────────────────⇥│               │
 #                  │                       │               │
 #                  │                       │ ╔═════════════╧══╗
-#                  │                       │ ║this is a note ░║
+#                  │                       │ ║this is a note  ║
 #                  │                       │ ╚═════════════╤══╝
 #                  │Is it ok               │ ╔═════════════╧════╗
-#                  │with a message that is │ ║This other note  ░║
+#                  │with a message that is │ ║This other note   ║
 #                  │on several lines?      │ ║should work       ║
 #                  │<──────────────────────│ ║on several lines  ║
 #                  │                       │ ╚═════════════╤════╝
@@ -32,7 +33,7 @@ from curses.textpad import Textbox, rectangle
 #                  │──────────────────────────────────────>│
 #                  │                       │               │
 #                  │                       ╔══════════════╗│────┐
-#                  │                       ║this is      ░║│    │ working in progress
+#                  │                       ║this is       ║│    │ working in progress
 #                  │                       ║another note  ║│<───┘
 #                  │                       ╚══════════════╝│
 #                  │                       │               │─ ─ ┐
@@ -67,7 +68,7 @@ from curses.textpad import Textbox, rectangle
 # ║                │<──────────────────────────────────────│            ║
 # ║                │                       │               │            ║
 # ║                │                    ╔══╧═══════════════╧══╗         ║
-# ║                │                    ║This is a long note ░║         ║
+# ║                │                    ║This is a long note  ║         ║
 # ║                │                    ║over Alice and Last  ║         ║
 # ║                │                    ╚══╤═══════════════╤══╝         ║
 # ║                │                 None  │               │            ║
@@ -92,17 +93,51 @@ class Shape:
 
 
 class Box:
-    def __init__(self,x,y,w,h):
+    def __init__(self,parent,h,w,y,x):
 
+        self.parent = parent
         # lines, columns, start line, start column
-        self.window = curses.newwin(h, w, y, x)
-
+        self.window = curses.newwin(h, w, y, x);
+        self.addBorder()
+        #self.window.border("│","│","─","─","┌","┐","└","┘")
+        self.innerWindow = self.window.subwin(h-2, w-2, y+1, x+1 );
+        #self.innerWindow.border('l','r','t','b','q','p','z','m');
         # Long strings will wrap to the next line automatically
         # to stay within the window
-        self.window.addstr(4, 4, "Hello from 4,4")
-        self.window.addstr(5, 15, "Hello from 5,15 with a long string")
+        self.innerWindow.addstr(4, 4, "Hello from 4,4")
+        self.innerWindow.addstr(5, 15, "Hello from 5,15 with a long string")
 
         ## Print the window to the screen
+        self.window.refresh()
+
+    def addBorder(self):
+        try:
+            height,width = self.window.getmaxyx()
+            height -= 1
+            width -= 1
+
+            for x in range(1,width):
+                self.window.addstr(0,x,"─")
+                self.window.addstr(height,x,"─")
+
+            for y in range(1,height):
+                self.window.addstr(y,0,"│")
+                self.window.addstr(y,width,"│")
+
+            self.window.addstr(0,0,"┌")
+            self.window.addstr(0,width,"┐")
+            self.window.addstr(height,0,"└")
+            self.window.addstr(height,width,"┘")
+
+        except curses.error:
+            pass  #stupid bug in curses after writing to the last character of a window
+
+
+    def move(self,y,x):
+        print("x="+str(x)+" y="+str(y))
+        self.parent.clear()
+        self.window.mvwin(y,x)
+        self.parent.refresh()
         self.window.refresh()
 
 
@@ -111,10 +146,19 @@ class Box:
 def main(stdscr):
 
     screen = curses.initscr()
+    curses.curs_set(0)
 
     screen.clear()
-    box = Box(10,0,20,15)
+    box = Box(screen,15,26,3,10)
+
+    while True:
+        for deg in range(0,360,1):
+            box.move(8+int(8*math.sin(math.radians(deg))),40+int(40*math.cos(math.radians(deg))))
+            curses.napms(5)
+
+    #box.move(5,11)
     curses.napms(2000)
+    #screen.getkey()
     #drawIt()
     ## The `screen` is a window that acts as the master window
     ## that takes up the whole screen. Other windows created
