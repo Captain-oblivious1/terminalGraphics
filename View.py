aedc1981d1898ce2d1abac384859eb20898484ba
Duane Knesek <duane.knesek@gmail.com>
2019-11-27 22:50:22 -0600

    started adding slicker character binary operations

--- View.py
+++ View.py
@@ -9,6 +9,92 @@ from ConnectorComponent import *
 from curses import wrapper
 #import keyboard
 
+#            " "   " "   " "   " "
+#            "╴"   "╶"   "╵"   "╷"
+#            "╸"   "╺"   "╹"   "╻"
+# boxArray [left][right][top][bottom]
+boxArray = [  
+  [ [ [ " ", "╷", "╻"], [ "╵", "│", "╽"], [ "╹", "╿", "┃"] ],
+    [ [ "╶", "┌", "┎"], [ "└", "├", "┟"], [ "┖", "┞", "┠"] ],
+    [ [ "╺", "┍", "┏"], [ "┕", "┝", "┢"], [ "┗", "┡", "┣"] ] ],
+  [ [ [ "╴", "┐", "┒"], [ "┘", "┤", "┧"], [ "┚", "┦", "┨"] ],
+    [ [ "─", "┬", "┰"], [ "┴", "┼", "╁"], [ "┸", "╀", "╂"] ],
+    [ [ "╼", "┮", "┲"], [ "┶", "┾", "╆"], [ "┺", "╄", "╊"] ] ],
+  [ [ [ "╸", "┑", "┓"], [ "┙", "┥", "┧"], [ "┛", "┩", "┫"] ],
+    [ [ "╾", "┭", "┱"], [ "┵", "┽", "╅"], [ "┹", "╃", "╉"] ],
+    [ [ "━", "┯", "┳"], [ "┷", "┿", "╈"], [ "┻", "╇", "╋"] ] ] ]
+
+# I made up a scheme that allows me to create box characters by 
+# or-ing and and-ing other characters.  I assign each chacater a
+# value based an integer that has "pixels" activated.  The follow
+# indicates what bits represent what pixel.
+#    ┌──┰──┬──┐      All box characters can be created by bitwise
+#    │21┃20│19│      or-ing any of the following:
+# ┌──┼──╂──┼──┼──┐   " " = 0x0
+# │18│17┃16│15│14│   "╴" = 0x1C00
+# ├──╁──╀──┼──┼──┤   "╸" = 0x39CE0
+# │13┃12│11│10│ 9│   "╶" = 0x700
+# ┟──╀──┼──┼──╁──┤   "╺" = 0xE738
+# ┃ 8│ 7│ 6│ 5┃ 4│   "╵" = 0x88400
+# ┖──┼──┼──┼──╂──┘   "╹" = 0x1DCE00
+#    │ 3│ 2│ 1┃      "╷" = 0x422
+#    └──┴──┴──┚      "╻" = 0xE77
+# (I blew off the double line versions because there are no
+# unicode characters that mix and match them.  For example
+# I can simulate or-ing "│" with "━" by using the "┿" char.
+# But there is no equivilent for the or of "║" and "━". Only
+# "║" and "─" (which is "╫").  I think thicker lines is more
+# useful than double lines.  Double lines look too DOSy.
+
+charToHexMap = {}
+hexToCharMap = {}
+
+def intializeMaps():
+    leftArray =   [ 0x0, 0x1C00,  0x39CE0  ]
+    rightArray =  [ 0x0, 0x700,   0xE738   ]
+    topArray =    [ 0x0, 0x88400, 0x1DCE00 ]
+    bottomArray = [ 0x0, 0x422,   0xE77    ]
+
+    for left in range(3):
+        for right in range(3):
+            for top in range(3):
+                for bottom in range(3):
+                    value = leftArray[left] | rightArray[right] | topArray[top] | bottomArray[bottom]
+                    char = boxArray[left][right][top][bottom]
+                    charToHexMap[char] = value
+                    hexToCharMap[value] = char
+
+intializeMaps()
+
+def charToHex(char):
+    return charToHexMap[char]
+
+def hexToChar(hex):
+    return hexToCharMap[hex]
+
+def orChars(char1,char2):
+    print("or-ing '"+char1+"' and '"+char2+"' -> '"+hexToChar(charToHex(char1) | charToHex(char2) ) +"'")
+
+orChars("─","│")
+orChars("┎","┘")
+deleteRight = 0x1B9CE6
+print("created Char='"+hexToChar(charToHex("─") & deleteRight | charToHex("│"))+"'")
+#print("created Char='"+hexToChar(charToHex("┎") & deleteRight | charToHex("│"))+"'")
+
+# unicode order:
+# "─", "━", "│" "┃",
+# "┌", "┍", "┎", "┏", "┐", "┑", "┒", "┓",
+# "└", "┕", "┖", "┗", "┘", "┙", "┚", "┛",
+# "├", "┝", "┞", "┟", "┠", "┡", "┢", "┣",
+# "┤", "┥", "┦", "┧", "┨", "┩", "┪", "┫",
+# "┬", "┭", "┮", "┯", "┰", "┱", "┲", "┳",
+# "┴", "┵", "┶", "┷", "┸", "┹", "┺", "┻",
+# "┼", "┽", "┾", "┿", "╀", "╁", "╂", "╃",
+# "╄", "╅", "╆", "╇", "╈", "╉", "╊", "╋",
+# "╴", "╵", "╶", "╷", 
+# "╸", "╹", "╺", "╻", 
+# "╼", "╽", "╾", "╿"}
+
 
 class Context:
     def __init__(self,window):
@@ -52,6 +138,8 @@ class Context:
         else:
             char = default
         self.addString(x,y,char,isBold)
+
+    #def drawBoxChar(self,x,y,
 
     #                 ─        │        ┌        ┐        └        ┘        ├        ┤        ┬        ┴        ┼
     verticalMap   = {"─":"┼",          "┌":"├", "┐":"┤", "└":"├", "┘":"┤", "├":"├", "┤":"┤", "┬":"┼", "┴":"┼", "┼":"┼"}
