d100d65539e184335a2533fde88e5c5698a9bb1f
Duane <duane.knesek@gmail.com>
2024-01-14 19:04:16 -0600

    Added table text editing.  Cannot yet add or delete columns or rows.

--- src/TableComponent.py
+++ src/TableComponent.py
@@ -1,9 +1,11 @@
 from Component import *
+from Menu import *
 
 class TableComponent(Component):
     def __init__(self,parent,tableElement):
         super().__init__(parent)
         self.tableElement = tableElement
+        self.editing = None
 
     def getRect(self):
         element = self.tableElement
@@ -100,8 +102,125 @@ class TableComponent(Component):
         self.tableElement.location += offset
 
     def showContextMenu(self,point,context):
-        options = ["stop editing","","split","join"]
+        if self.editing is not None:
+            options = ["stop editing"]
+        else:
+            options = ["edit"]
+        options += [""]
+
+        col,row = self._fieldAt(point)
+        if col is not None and row is not None:
+            just = self.tableElement.dataRows[row][col].justification 
+            if just!=Justification.LEFT:
+                options += ["left justify"]
+            if just!=Justification.CENTER:
+                options += ["center justify"]
+            if just!=Justification.RIGHT:
+                options += ["right justify"]
+
         self.getDiagramComponent().showMenu(Menu(self,options,point,self.menuResult))
 
     def menuResult(self,menu):
-        print("Chose menu option '"+menu.getSelectedOption()+"'")
+        option = menu.getSelectedOption()
+        if option=="left justify":
+            just = Justification.LEFT
+        elif option=="center justify":
+            just = Justification.CENTER
+        elif option=="right justify":
+            just = Justification.RIGHT
+        else:
+            just = None
+
+        col,row = self._fieldAt(menu.getTopLeft())
+        if just is not None:
+            self.tableElement.dataRows[row][col].justification = just
+            self.invalidate()
+        else:
+            editor = self.getEditor()
+            if option=="edit":
+                if row is not None and col is not None:
+                    self.editing = [row,col]
+                    editor.addKeyListener(self)
+                    self._changeText('')
+                    self.invalidate()
+            else:
+                self._stopEditing()
+
+    def keyEvent(self,event):
+        if event>=0 and event<=255:
+            if(event==27):
+                self._stopEditing()
+            else:
+                self._changeText( chr(event), True )
+        else:
+            if event==263: # Backspace
+                self._changeText( self._editingField().text[0:-1], False )
+
+    def _editingField(self):
+        if self.editing==None:
+            return None
+        else:
+            row = self.editing[0]
+            col = self.editing[1]
+            return self.tableElement.dataRows[row][col]
+
+    def _stopEditing(self):
+        self.getEditor().removeKeyListener(self)
+        self.editing = None
+        self.invalidate()
+
+    def _changeText(self,ch,append=False):
+        self.invalidate()
+        tableElement = self.tableElement
+        row = self.editing[0]
+        col = self.editing[1]
+        editingField = self._editingField()
+        if append:
+            editingField.text += ch
+        else:
+            editingField.text = ch
+
+        widestCell = 0
+        for i in range(len(tableElement.columnWidths)):
+            longestLine,_ = longestLineAndNumberLines(tableElement.dataRows[i][col].text)
+            #print("longestLine for '"+tableElement.dataRows[i]
+            if longestLine>widestCell:
+                widestCell = longestLine
+
+        tallestCell = 0
+        for i in range(len(tableElement.rowHeights)):
+            _,nLines = longestLineAndNumberLines(tableElement.dataRows[row][i].text)
+            if nLines>tallestCell:
+                tallestCell = nLines
+
+        #print("widest="+str(widestCell)+" tallest="+str(tallestCell))
+        tableElement.columnWidths[col] = widestCell
+        tableElement.rowHeights[row] = tallestCell
+
+        self.invalidate()
+
+    def _fieldAt(self,point):
+        rect = self.getRect()
+        x = rect.l
+        element = self.tableElement
+        widths =  element.columnWidths
+        nWidths = len(widths)
+        col = None
+        for i in range(nWidths):
+            x += widths[i] + 1
+            if point.x<x:
+                col = i
+                break
+
+        heights =  element.rowHeights
+        nHeights = len(heights)
+        y = rect.t
+
+        row = None
+        for i in range(nHeights):
+            y += heights[i] + 1
+            if point.y<y:
+                row = i
+                break
+
+        return col,row
--- src/Util.py
+++ src/Util.py
@@ -10,3 +10,24 @@ def vectorToDirection(vector):
     offset = normed + Point(1,1)
     return  _vToDArray[offset.y][offset.x]
 
+def longestLineFor(text):
+    longest,_ = longestLineAndNumberLines(text)
+    return longest
+
+def longestLineAndNumberLines(text):
+    longest = 0
+    current = 0
+    nLines = 1
+    for ch in text:
+        if ch=='\n':
+            nLines += 1
+            if current>longest:
+                longest = current
+            current = 0
+        else:
+            current += 1
+    if current>longest:
+        longest = current
+
+    return longest,nLines
+
